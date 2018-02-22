8389864e687e0b40028c0ce2e3067b2e5cd46707
prasannaakhil <prasanna.akhil004@gmail.com>
2018-02-22 16:32:21 +0530

    basic c codes

---
+++ v-cone.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    float r,h,a;
+    printf("enter value of r \n");
+    scanf("%f",&r);
+    printf("enter value of h \n");
+    scanf("%f",&h);
+    a=(3.14*r*r*h)/3;
+    printf("volue of cone is %f \n",a);
+    return 0;
+}
---
+++ v-sphere.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float a,r;
+    printf("enter value of r \n");
+    scanf("%f",&r);
+    a=(4*(3.14)*(r*r*r))/3;
+    printf("volue of sphere is %f \n",a);
+    return 0;
+}
+
+
+
+
