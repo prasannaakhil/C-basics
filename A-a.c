d5feb9500f44905cab0e3ecd5501cd4040a15772
prasannaakhil <prasanna.akhil004@gmail.com>
2018-02-24 16:15:52 +0530

    simple if statements

--- A-a.c
+++ A-a.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+    char a;
+    printf("enter any alphabet \n");
+    scanf("%c",&a);
+    if(a<97)
+        printf("%c \n",a+32);
+    else
+        printf("%c \n",a-32);
+}
